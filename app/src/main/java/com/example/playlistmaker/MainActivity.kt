7fd99412a26018f6a27aff20b5d966b10aa410e0
kotzimaru <kotzimaru@yandex.ru>
2022-11-24 20:09:52 +0300

    Ок1

---
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.playlistmaker
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import  androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.LinearLayout
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        val searchButton = findViewById<Button>(R.id.search_button)
+        val mediaButton = findViewById<Button>(R.id.media_button)
+        val settingsButton = findViewById<Button>(R.id.settings_button)
+
+        searchButton.setOnClickListener {
+            val searchDisplayIntent = Intent(this, SearchActivity::class.java)
+            startActivity(searchDisplayIntent)}
+
+        mediaButton.setOnClickListener {
+            val mediaDisplayIntent = Intent(this, MediaActivity::class.java)
+            startActivity(mediaDisplayIntent)}
+
+
+        settingsButton.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Еще не работает", Toast.LENGTH_SHORT).show()}
+
+
+
+
+    }
+}
+
